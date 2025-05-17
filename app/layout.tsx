b6f6803e363177134bb90ba99cd5ea85a474f8f4
BM-Tae <tae@blockmajority.io>
2025-05-17 22:56:33 +1000

    tailwind config + navbar wip

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${montserrat.variable} antialiased`}>
+      <body className={`${montserrat.variable} antialiased bg-background`}>
         <Navbar />
         {children}
       </body>
--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -1,7 +1,27 @@
 import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="w-full flex justify-between px-10 py-4 shadow-md">
+      <div className="flex items-center gap-12 text-xl font-medium">
+        <Link href="/">
+          <Image src="/" alt="HPSA Logo" width={50} height={50} />
+        </Link>
+        <Link href="/" className="hover:text-primary duration-200 ">Home</Link>
+        <Link href="/" className="hover:text-primary duration-200 ">About</Link>
+        <Link href="/" className="hover:text-primary duration-200 ">Courses</Link>
+        <Link href="/" className="hover:text-primary duration-200 ">Contact</Link>
+      </div>
+
+      <div className="flex items-center gap-2 text-xl font-medium">
+        <Button className="text-xl hover:cursor-pointer duration-200">Sign In</Button>
+        <Button className="text-xl hover:cursor-pointer duration-200">Sign Up</Button>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
