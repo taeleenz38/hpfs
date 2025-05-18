d62135b028124899d4c4b4c3a39998147dab73d5
BM-Tae <tae@blockmajority.io>
2025-05-18 19:20:52 +1000

    navbar wip

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -5,20 +5,32 @@ import { Button } from "@/components/ui/button";
 
 const Navbar = () => {
   return (
-    <div className="w-full flex justify-between px-10 py-4 shadow-md">
-      <div className="flex items-center gap-12 text-xl font-medium">
-        <Link href="/">
-          <Image src="/" alt="HPSA Logo" width={50} height={50} />
-        </Link>
-        <Link href="/" className="hover:text-primary duration-200 ">Home</Link>
-        <Link href="/" className="hover:text-primary duration-200 ">About</Link>
-        <Link href="/" className="hover:text-primary duration-200 ">Courses</Link>
-        <Link href="/" className="hover:text-primary duration-200 ">Contact</Link>
-      </div>
+    <div className="w-full flex justify-between px-24 py-6 shadow-md">
+      <Link href="/">
+        <Image src="/images/LOGO.svg" alt="HPSA Logo" width={75} height={75} />
+      </Link>
 
+      <div className="flex items-center gap-16 text-2xl font-medium">
+        <Link href="/" className="hover:text-primary duration-200">
+          Home
+        </Link>
+        <Link href="/" className="hover:text-primary duration-200">
+          About
+        </Link>
+        <Link href="/" className="hover:text-primary duration-200">
+          Courses
+        </Link>
+        <Link href="/" className="hover:text-primary duration-200">
+          Contact
+        </Link>
+      </div>
       <div className="flex items-center gap-2 text-xl font-medium">
-        <Button className="text-xl hover:cursor-pointer duration-200">Sign In</Button>
-        <Button className="text-xl hover:cursor-pointer duration-200">Sign Up</Button>
+        <Button className="text-xl hover:cursor-pointer duration-200">
+          Sign In
+        </Button>
+        <Button className="text-xl hover:cursor-pointer duration-200">
+          Sign Up
+        </Button>
       </div>
     </div>
   );
